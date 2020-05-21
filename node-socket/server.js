dbc0970ce7a305bb732dbe5b36ab37d4cdcf95c5
Nazim <nazim@adaptabiz.com>
2020-05-22 04:49:30 +0530

    setup node

---
+++ node-socket/server.js
@@ -0,0 +1,124 @@
+
+var fs = require('fs');
+const stream = require('./stream');
+var streamSql = new stream;
+// don't forget to use your own keys!
+var options = {
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+};
+
+// HTTPs server
+var app = require('https').createServer(options, function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.write("<h1>Live Stream Server Working.</h1>");
+    response.end();
+});
+
+
+// socket.io goes below
+
+var io = require('socket.io').listen(app, {
+    log: false,
+    origins: '*:*'
+});
+
+io.set('transports', [
+    // 'websocket',
+    'xhr-polling',
+    'jsonp-polling'
+]);
+
+var channels = {};
+
+io.sockets.on('connection', function (socket) {
+    var initiatorChannel = '';
+    var userDetails = false;
+    var streamDetails
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+
+    socket.on('new-channel', async function (data) {
+        socket.emit('response', 'Initializing');
+        if (!channels[data.channel] || userDetails === false) {
+            initiatorChannel = data.channel;
+//            userDetails = await streamSql.getUser(data.email, data.sender);
+//            if (userDetails === false) {
+//                console.log('User Not Exist');
+//                socket.emit('response', 'User Not Exist');
+//                socket.emit('disconnect');
+//                return false;
+//            }
+        } else {
+            console.log('Already User');
+        }
+//        streamDetails = await streamSql.getStream(data.channel, userDetails.id);
+//        if (streamDetails === false) {
+//            console.log('Stream Not Exist');
+//            socket.emit('response', 'Stream Not Exist');
+//            socket.emit('disconnect');
+//            return false;
+//        }
+//        if (!channels[data.channel]) {
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender, streamDetails);
+//        }
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !!channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        console.log('disconnect');
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
+});
+
+function onNewNamespace(channel, sender, streamDetails) {
+    io.of('/' + channel).on('connection', function (socket) {
+        var username;
+        var initialStream = true;
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+
+        socket.on('message', function (data) {
+            if (data.sender == sender) {
+                if (!username) {
+                    username = data.data.sender;
+//                    if (initialStream) {
+//                        streamSql.initialStream(data.data.roomToken, data.data.broadcaster, streamDetails.id);
+//                        initialStream = false;
+//                    }
+                }
+                socket.broadcast.emit('message', data.data);
+            }
+        });
+
+        socket.on('disconnect', function () {
+            console.log('disconnet user');
+            if (username) {
+                socket.broadcast.emit('user-left', username);
+                username = null;
+            }
+        });
+    });
+}
+
+// run app
+
+app.listen(process.env.PORT || 9559);
+
+process.on('unhandledRejection', (reason, promise) => {
+    process.exit(1);
+});
+
+console.log('Please open SSL URL: https://localhost:' + (process.env.PORT || 9559) + '/');
