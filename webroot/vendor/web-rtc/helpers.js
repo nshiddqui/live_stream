483e8af10620f16ed581482132657182d573ded0
Nazim <nazim@adaptabiz.com>
2020-06-02 21:32:19 +0530

    done changes

--- webroot/vendor/web-rtc/helpers.js
+++ webroot/vendor/web-rtc/helpers.js
@@ -123,11 +123,15 @@ export default {
 //                    ]
 //                }
                 {
-                    "urls": [
-                        "turn:13.250.13.83:3478"
-                    ],
-                    "username": "YzYNCouZM1mhqhmseWk6",
-                    "credential": "YzYNCouZM1mhqhmseWk6"
+                    urls: ["stun:eu-turn4.xirsys.com"]
+                },
+                {
+                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    urls: [
+                        "turn:eu-turn4.xirsys.com:80?transport=udp",                         
+                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                    ]
                 }
             ]
         };
