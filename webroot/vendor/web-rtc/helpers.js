a90055058a52a3ecaa319feb8e9b0e223f7f6ca3
Nazim <nazim@adaptabiz.com>
2020-06-02 20:55:37 +0530

    done changes

--- webroot/vendor/web-rtc/helpers.js
+++ webroot/vendor/web-rtc/helpers.js
@@ -1,91 +1,82 @@
 export default {
-    generateRandomString(){
+    generateRandomString() {
         return Math.random().toString(36).slice(2).substring(0, 15);
     },
 
-
-    closeVideo(elemId){
-        if(document.getElementById(elemId)){
+    closeVideo(elemId) {
+        if (document.getElementById(elemId)) {
             document.getElementById(elemId).remove();
             this.adjustVideoElemSize();
         }
     },
 
-
-    pageHasFocus(){
+    pageHasFocus() {
         return !(document.hidden || document.onfocusout || window.onpagehide || window.onblur);
     },
 
-
-    getQString(url='', keyToReturn=''){
+    getQString(url = '', keyToReturn = '') {
         url = url ? url : location.href;
         let queryStrings = decodeURIComponent(url).split('#', 2)[0].split('?', 2)[1];
-        
-        if(queryStrings){
+
+        if (queryStrings) {
             let splittedQStrings = queryStrings.split('&');
-            
-            if(splittedQStrings.length){
+
+            if (splittedQStrings.length) {
                 let queryStringObj = {};
-                
-                splittedQStrings.forEach(function(keyValuePair){
+
+                splittedQStrings.forEach(function (keyValuePair) {
                     let keyValue = keyValuePair.split('=', 2);
-                    
-                    if(keyValue.length){
+
+                    if (keyValue.length) {
                         queryStringObj[keyValue[0]] = keyValue[1];
                     }
-                });            
-                
+                });
+
                 return keyToReturn ? (queryStringObj[keyToReturn] ? queryStringObj[keyToReturn] : null) : queryStringObj;
             }
-            
+
             return null;
         }
-        
+
         return null;
     },
 
-
-    userMediaAvailable(){
+    userMediaAvailable() {
         return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
     },
 
-
-    getUserFullMedia(){
-        if(this.userMediaAvailable()){
+    getUserFullMedia() {
+        if (this.userMediaAvailable()) {
             return navigator.mediaDevices.getUserMedia({
-                video: true, 
+                video: {
+                    width: {min: 500, ideal: 800, max: 900},
+                    height: {min: 500, ideal: 800, max: 900},
+                },
                 audio: {
                     echoCancellation: true,
                     noiseSuppression: true
                 }
             });
-        }
-
-        else{
+        } else {
             throw new Error('User media not available');
         }
     },
 
-
-    getUserAudio(){
-        if(this.userMediaAvailable()){
+    getUserAudio() {
+        if (this.userMediaAvailable()) {
             return navigator.mediaDevices.getUserMedia({
                 audio: {
                     echoCancellation: true,
                     noiseSuppression: true
                 }
             });
-        }
-
-        else{
+        } else {
             throw new Error('User media not available');
         }
     },
 
-
-
-    shareScreen(){
-        if(this.userMediaAvailable()){
+    shareScreen() {
+        if (this.userMediaAvailable()) {
             return navigator.mediaDevices.getDisplayMedia({
                 video: {
                     cursor: "always"
@@ -96,42 +87,39 @@ export default {
                     sampleRate: 44100
                 }
             });
-        }
-
-        else{
+        } else {
             throw new Error('User media not available');
         }
     },
 
-
-    getIceServer(){
+    getIceServer() {
         // "turns:eu-turn4.xirsys.com:5349?transport=tcp"
         // "turns:eu-turn4.xirsys.com:443?transport=tcp"
         // "turn:eu-turn4.xirsys.com:80?transport=tcp",
         // "turn:eu-turn4.xirsys.com:3478?transport=udp",
         return {
             iceServers: [
-                {url:"stun:stun.l.google.com:19302"},
-                {url:"stun:stun1.l.google.com:19302"},
-                {url:"stun:stun2.l.google.com:19302"},
-                {url:"stun:stun3.l.google.com:19302"},
-                {url:"stun:stun4.l.google.com:19302"},
-                {url:"stun:stun01.sipphone.com"},
-                {url:"stun:stun.ekiga.net"},
-                {url:"stun:stun.fwdnet.net"},
-                {url:"stun:stun.ideasip.com"},
-                {url:"stun:stun.iptel.org"},
-                {url:"stun:stun.rixtelecom.se"},
-                {url:"stun:stun.schlund.de"},
-                {url:"stun:stunserver.org"},
-                {url:"stun:stun.softjoys.com"},
-                {url:"stun:stun.voiparound.com"},
-                {url:"stun:stun.voipbuster.com"},
-                {url:"stun:stun.voipstunt.com"},
-                {url:"stun:stun.voxgratia.org"},
-                {url:"stun:stun.xten.com"},
+                {url: "stun:stun.l.google.com:19302"},
+//                {url:"stun:stun1.l.google.com:19302"},
+//                {url:"stun:stun2.l.google.com:19302"},
+//                {url:"stun:stun3.l.google.com:19302"},
+//                {url:"stun:stun4.l.google.com:19302"},
+//                {url:"stun:stun01.sipphone.com"},
+//                {url:"stun:stun.ekiga.net"},
+//                {url:"stun:stun.fwdnet.net"},
+//                {url:"stun:stun.ideasip.com"},
+//                {url:"stun:stun.iptel.org"},
+//                {url:"stun:stun.rixtelecom.se"},
+//                {url:"stun:stun.schlund.de"},
+//                {url:"stun:stunserver.org"},
+//                {url:"stun:stun.softjoys.com"},
+//                {url:"stun:stun.voiparound.com"},
+//                {url:"stun:stun.voipbuster.com"},
+//                {url:"stun:stun.voipstunt.com"},
+//                {url:"stun:stun.voxgratia.org"},
+//                {url:"stun:stun.xten.com"},
                 {
-                    username: "shelty", 
+                    username: "shelty",
                     credential: "nazim@123",
                     urls: [
                         "turn:103.250.186.37:3478",
@@ -140,15 +128,14 @@ export default {
             ]
         };
     },
-    
-    
-    addChat(data, senderType){
+
+    addChat(data, senderType) {
         let chatMsgDiv = document.querySelector('#chat-messages');
         let contentAlign = 'justify-content-end';
         let senderName = 'You';
         let msgBg = 'bg-white';
 
-        if(senderType === 'remote'){
+        if (senderType === 'remote') {
             contentAlign = 'justify-content-start';
             senderName = data.sender;
             msgBg = '';
@@ -162,7 +149,7 @@ export default {
 
         let colDiv = document.createElement('div');
         colDiv.className = `col-10 card chat-card msg ${msgBg}`;
-        colDiv.innerHTML =  data.msg;
+        colDiv.innerHTML = data.msg;
 
         let rowDiv = document.createElement('div');
         rowDiv.className = `row ${contentAlign} mb-2`;
@@ -178,55 +165,44 @@ export default {
          * 1. Page has focus
          * 2. User has not moved scrollbar upward. This is to prevent moving the scroll position if user is reading previous messages.
          */
-        if(this.pageHasFocus){
+        if (this.pageHasFocus) {
             rowDiv.scrollIntoView();
         }
     },
 
-
-    toggleChatNotificationBadge(){
-        if(document.querySelector('#chat-pane').classList.contains('chat-opened')){
+    toggleChatNotificationBadge() {
+        if (document.querySelector('#chat-pane').classList.contains('chat-opened')) {
             document.querySelector('#new-chat-notification').setAttribute('hidden', true);
-        }
-
-        else{
+        } else {
             document.querySelector('#new-chat-notification').removeAttribute('hidden');
         }
     },
 
-
-
-    replaceTrack(stream, recipientPeer){
+    replaceTrack(stream, recipientPeer) {
         let sender = recipientPeer.getSenders ? recipientPeer.getSenders().find(s => s.track && s.track.kind === stream.kind) : false;
-        
+
         sender ? sender.replaceTrack(stream) : '';
     },
 
-
-
-    toggleShareIcons(share){
+    toggleShareIcons(share) {
         let shareIconElem = document.querySelector('#share-screen');
-        
-        if(share){
+
+        if (share) {
             shareIconElem.setAttribute('title', 'Stop sharing screen');
             shareIconElem.children[0].classList.add('text-danger');
             shareIconElem.children[0].classList.remove('text-primary');
-        }
-
-        else{
+        } else {
             shareIconElem.setAttribute('title', 'Share screen');
             shareIconElem.children[0].classList.add('text-primary');
             shareIconElem.children[0].classList.remove('text-danger');
         }
     },
 
-
-    toggleVideoBtnDisabled(disabled){
+    toggleVideoBtnDisabled(disabled) {
         document.getElementById('toggle-video').disabled = disabled;
     },
 
-
-    maximiseStream(e){
+    maximiseStream(e) {
         let elem = e.target.parentElement.previousElementSibling;
 
         elem.requestFullscreen() || elem.mozRequestFullScreen() || elem.webkitRequestFullscreen() || elem.msRequestFullscreen();
@@ -245,86 +221,75 @@ export default {
         // }
     },
 
-
-    singleStreamToggleMute(e){
-        if(e.target.classList.contains('fa-microphone')){
+    singleStreamToggleMute(e) {
+        if (e.target.classList.contains('fa-microphone')) {
             e.target.parentElement.previousElementSibling.muted = true;
             e.target.classList.add('fa-microphone-slash');
             e.target.classList.remove('fa-microphone');
-        }
-
-        else{
+        } else {
             e.target.parentElement.previousElementSibling.muted = false;
             e.target.classList.add('fa-microphone');
             e.target.classList.remove('fa-microphone-slash');
         }
     },
 
-
-    saveRecordedStream(stream, user){
-        let blob = new Blob(stream, {type:'video/mp4'});
+    saveRecordedStream(stream, user) {
+        let blob = new Blob(stream, {type: 'video/mp4'});
 
         let file = new File([blob], `${user}-${moment().unix()}-record.mp4`);
 
         saveAs(file);
     },
 
-
-    toggleModal(id, show){
+    toggleModal(id, show) {
         let el = document.getElementById(id);
-        
-        if(show){             
+
+        if (show) {
             el.style.display = 'block';
             el.removeAttribute('aria-hidden');
-        }
-
-        else{
+        } else {
             el.style.display = 'none';
             el.setAttribute('aria-hidden', true);
         }
     },
 
-
-
-    setLocalStream(stream, mirrorMode=true){
+    setLocalStream(stream, mirrorMode = true) {
         const localVidElem = document.getElementById('local');
-        
+
         localVidElem.srcObject = stream;
         mirrorMode ? localVidElem.classList.add('mirror-mode') : localVidElem.classList.remove('mirror-mode');
     },
 
-
-    adjustVideoElemSize(){
+    adjustVideoElemSize() {
         let elem = document.getElementsByClassName('card');
         let totalRemoteVideosDesktop = elem.length;
         let newWidth = totalRemoteVideosDesktop <= 2 ? '50%' : (
-            totalRemoteVideosDesktop == 3 ? '33.33%' : (
-                totalRemoteVideosDesktop <= 8 ? '25%' : (
-                    totalRemoteVideosDesktop <= 15 ? '20%' : (
-                        totalRemoteVideosDesktop <= 18 ? '16%' : (
-                            totalRemoteVideosDesktop <= 23 ? '15%' : (
-                                totalRemoteVideosDesktop <= 32 ? '12%' : '10%'
-                            )
+                totalRemoteVideosDesktop == 3 ? '33.33%' : (
+                        totalRemoteVideosDesktop <= 8 ? '25%' : (
+                                totalRemoteVideosDesktop <= 15 ? '20%' : (
+                                        totalRemoteVideosDesktop <= 18 ? '16%' : (
+                                                totalRemoteVideosDesktop <= 23 ? '15%' : (
+                                                        totalRemoteVideosDesktop <= 32 ? '12%' : '10%'
+                                                        )
+                                                )
+                                        )
+                                )
                         )
-                    )
-                )
-            )
-        );
+                );
 
 
-        for(let i = 0; i < totalRemoteVideosDesktop; i++){
+        for (let i = 0; i < totalRemoteVideosDesktop; i++) {
             elem[i].style.width = newWidth;
             elem[i].style.height = newWidth;
         }
     },
 
-
-    createDemoRemotes(str, total=6){
+    createDemoRemotes(str, total = 6) {
         let i = 0;
 
-        let testInterval = setInterval(()=>{
+        let testInterval = setInterval(() => {
             let newVid = document.createElement('video');
-            newVid.id = `demo-${i}-video`;            
+            newVid.id = `demo-${i}-video`;
             newVid.srcObject = str;
             newVid.autoplay = true;
             newVid.className = 'remote-video';
@@ -334,14 +299,14 @@ export default {
             controlDiv.className = 'remote-video-controls';
             controlDiv.innerHTML = `<i class="fa fa-microphone text-danger pr-3 mute-remote-mic" title="Mute"></i>
                 <i class="fa fa-expand text-danger expand-remote-video" title="Expand"></i>`;
-            
+
             //create a new div for card
             let cardDiv = document.createElement('div');
             cardDiv.className = 'card card-sm';
             cardDiv.id = `demo-${i}`;
             cardDiv.appendChild(newVid);
             cardDiv.appendChild(controlDiv);
-            
+
             //put div in main-section elem
             document.getElementById('videos').appendChild(cardDiv);
 
@@ -349,7 +314,7 @@ export default {
 
             i++;
 
-            if(i == total){
+            if (i == total) {
                 clearInterval(testInterval);
             }
         }, 2000);
