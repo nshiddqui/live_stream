31e0269043e5c16edc80b560878ae9f9088674aa
Nazim <nazim@adaptabiz.com>
2020-06-29 08:17:24 +0530

    functionality done and fixed bug for UC-3766 and UC-3775

--- src/Controller/StreamsController.php
+++ src/Controller/StreamsController.php
@@ -44,7 +44,7 @@ class StreamsController extends AppController {
                     'contain' => ['Streams' => 'Users'],
                     'conditions' => [
                         'StreamDetails.user_id' => $auth_user_id,
-                        'Streams.end_time BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 2 MONTH)',
+                        'Streams.end_time BETWEEN NOW() AND DATE_SUB(NOW(), INTERVAL 2 MONTH)',
                     ]
                 ])
                 ->table('StreamDetails')
